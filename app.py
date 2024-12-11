be168dde4cbdd5522de186a3d54ef7119af08edd
rafaavmsilva <rafaavmsilva@gmail.com>
2024-12-11 17:29:42 -0300

    feat: set default port to 5001 for local development

--- app.py
+++ app.py
@@ -1013,4 +1013,5 @@ def print_comissoes():
         return redirect(url_for('comissoes'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
